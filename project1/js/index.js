557c8bfe6c200ad4d2116bbfb90cf5257c5a7af5
unknown <2366774842@qq.com>
2023-02-21 14:00:43 +0800

    first commit

---
+++ project1/js/index.js
@@ -0,0 +1,14 @@
+window.onload = function ()
+{
+    //1、当前元素
+    //2、数据 data.js -> goodData.path
+    //3、动态产生 数据渲染
+    //4、细节处理
+    var navPath = document.querySelector('#wapper #content .contentMain .navPath');
+    console.log(navPath);
+
+    var path= goodData.path;
+    console.log(path);
+
+
+}
